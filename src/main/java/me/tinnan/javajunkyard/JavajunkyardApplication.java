70a6432d1563e279935296a75d42375e18848a32
tin_nan@hotmail.com <tin_nan@hotmail.com>
2020-02-21 19:21:59 +0700

    First commit.

---
+++ src/main/java/me/tinnan/javajunkyard/JavajunkyardApplication.java
@@ -0,0 +1,13 @@
+package me.tinnan.javajunkyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajunkyardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajunkyardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/tinnan/javajunkyard/JavajunkyardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tinnan.javajunkyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajunkyardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
